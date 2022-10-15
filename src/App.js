2f23bb88c7e86d79f104e26c14dcc7f199fa3652
Asanda <noelmajola@gmail.com>
2022-10-15 15:39:57 +0200

    Adjust styling and content of h1 element.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <h1 style={{ display: "block", margin: "auto" }}>
-      Movie site coming soon..
-    </h1>
-  );
+  return <h1>Movie site coming soon..</h1>;
 }
 
 export default App;
