f0ad81a8211408b2d4614d0f42f11003b32b41b0
Asanda <noelmajola@gmail.com>
2022-10-15 23:35:16 +0200

    Create navigation component.

---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+function NavMenu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="rgba(0,0,0,0.9)" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img src={"/Assets/film_reel.png"} alt="movie reel" height="35" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/movies">
+              Movies
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavMenu;
