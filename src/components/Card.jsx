101c05d505e7038a416fe20ddfc12c390f6f2aef
Asanda <noelmajola@gmail.com>
2022-10-18 16:24:37 +0200

    Import animation css file.Add animation class to figure element.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
+import "animate.css";
+
 const Card = (props) => {
   return (
     <>
-      <figure style={{ width: "300px" }}>
+      <figure
+        style={{ width: "300px" }}
+        className="animate__animated animate__zoomIn"
+      >
         <img src={props.imgSrc} alt={props.alt} style={props.imgStyle} />
         <figcaption>
           <h1 style={{ fontSize: "large" }}>{props.title}</h1>
