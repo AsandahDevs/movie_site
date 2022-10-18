dd3cc661d8d070a9501cf344f6e7a8e0e6e85a4f
Asanda <noelmajola@gmail.com>
2022-10-18 21:49:05 +0200

    Add logic to filter movies by title.Import bootstrap form component.

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
 import useFetch from "../hooks/useFetch";
+import Form from "react-bootstrap/Form";
 
 const Movies = () => {
   const [imageUrl, setImageUrl] = useState("");
+  const [movieName, setMovieName] = useState("");
 
   // using a custom hook to fetch movie data.
   const movies = useFetch(
@@ -20,26 +22,46 @@ const Movies = () => {
       .catch((error) => console.log(error.message));
   }, []);
 
+  const captureMovieTitle = (e) => {
+    setMovieName(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) => {
+    return movie.title.toLowerCase().includes(movieName.toLowerCase());
+  });
+
   return (
     <>
       <h1>Most popular movies</h1>
+      <Form.Control
+        type="search"
+        placeholder="search movie.."
+        value={movieName}
+        style={{ width: "300px", display: "block", margin: "auto" }}
+        onChange={captureMovieTitle}
+      />
+      <br />
       <div className="grid-layout">
-        {movies.map((movie) => {
-          return (
-            <Card
-              key={movie.id}
-              /*Appending poster path of each image with its base URL from the configuration files.
+        {filteredMovies.length === 0 ? (
+          <p style={{ color: "white" }}>no matches found</p>
+        ) : (
+          filteredMovies.map((movie) => {
+            return (
+              <Card
+                key={movie.id}
+                /*Appending poster path of each image with its base URL from the configuration files.
             This is done to create a fully working image file path.*/
-              imgSrc={imageUrl.concat("w500", movie.poster_path)}
-              alt={movie.title}
-              imgStyle={{
-                width: "100%",
-                height: "auto",
-              }}
-              title={movie.title}
-            />
-          );
-        })}
+                imgSrc={imageUrl.concat("w500", movie.poster_path)}
+                alt={movie.title}
+                imgStyle={{
+                  width: "100%",
+                  height: "auto",
+                }}
+                title={movie.title}
+              />
+            );
+          })
+        )}
       </div>
     </>
   );
