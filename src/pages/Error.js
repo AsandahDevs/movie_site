79887a430b5f2147f62abab08dddd2490586f573
Asanda <noelmajola@gmail.com>
2022-10-19 14:14:29 +0200

    Create error component for handling invalid route paths.

---
+++ src/pages/Error.js
@@ -0,0 +1,25 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+const Error = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <h2
+        style={{
+          color: "whitesmoke",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Page not found !
+      </h2>
+      <Button style={{ display: "block", margin: "auto" }} variant="secondary">
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          Go back home
+        </Link>
+      </Button>
+    </>
+  );
+};
+
+export default Error;
