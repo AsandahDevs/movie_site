c91e9600188a405dfcdf0877da87e8dc404e523e
Asanda <noelmajola@gmail.com>
2022-10-21 17:20:34 +0200

    Update home content to show trending movies for the week.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,50 @@
-const Home = () => {
-  return <h1>This is homepage</h1>;
-};
+import Card from "../components/Card";
+import useFetch from "../hooks/useFetch";
+import useImageFiles from "../hooks/useImageFiles";
+import Spinner from "react-bootstrap/Spinner";
 
+const Home = () => {
+  /* using a custom hook to fetch movie data.Please note this hook returns an object with two properties,
+  'info'(which is an array of data), and 'loadingStatus' (which is a boolean value).*/
+  const { info, loadingStatus } = useFetch(
+    "https://api.themoviedb.org/3/trending/movie//week?api_key=73585424b9d1198974dbb05a54c359df&language=en-US"
+  );
+
+  // using a custom hook to fetch the base image URL for movie posters from  TMBD's image configuration files.
+  const imageURL = useImageFiles();
+
+  return (
+    <>
+      <h1>Trending movies</h1>
+
+      <div className="grid-layout">
+        {loadingStatus ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : info.length === 0 ? (
+          <p>no matches found</p>
+        ) : (
+          info.map((movie) => {
+            return (
+              <Card
+                key={movie.id}
+                /*Appending poster path of each image with its base URL from the configuration files.
+            This is done to create a fully working image file path.*/
+                imgSrc={imageURL.concat("w500", movie.poster_path)}
+                alt={movie.title}
+                imgStyle={{
+                  width: "100%",
+                  height: "auto",
+                }}
+                releaseYear={movie.release_date}
+                title={movie.title}
+              />
+            );
+          })
+        )}
+      </div>
+    </>
+  );
+};
 export default Home;
