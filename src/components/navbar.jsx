b33e3f4153052d7bfb1500dfb62746c24cb28885
Asanda <noelmajola@gmail.com>
2022-10-21 21:47:53 +0200

    Update navigation links and image links .

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,15 +8,21 @@ function NavMenu() {
     <Navbar collapseOnSelect expand="lg" bg="rgba(0,0,0,0.9)" variant="dark">
       <Container fluid>
         <Navbar.Brand href="#">
-          <img src={"/Assets/film_reel.png"} alt="movie reel" height="35" />
+          <img
+            src={
+              "https://asandahdevs.github.io/movie_site/Assets/film_reel.png"
+            }
+            alt="movie reel"
+            height="35"
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">
+            <Nav.Link as={NavLink} to="/movie_site/">
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/movies">
+            <Nav.Link as={NavLink} to="movies">
               Movies
             </Nav.Link>
           </Nav>
