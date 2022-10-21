ecf4931d8410ddafe36284da9606c4df8a504c83
Asanda <noelmajola@gmail.com>
2022-10-21 11:27:52 +0200

    Revert "Add state for error messages."
    This reverts commit c72a5ca16cb05a58e2aeaba4ed0fe3ecdf88ec07, which involved adding state for storing client error messages.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,23 +4,17 @@ import { useState } from "react";
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loadStatus, setLoadStatus] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [errState, setErrState] = useState(false);
-
   useEffect(() => {
     setLoadStatus(true);
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data.results)) // since all api data have a results property,which is an array of objects,we are accessing it and storing it onto our state.
       .then(() => setLoadStatus(false))
-      .catch((err) => setErrorMessage(err.message))
-      .catch(() => setErrState(true));
+      .catch((err) => console.log(err.message));
   }, [url]); // the url passed into the dependency array means that if our url changes, the hook needs to execute again to fetch new data.
   return {
     info: data,
     loadingStatus: loadStatus,
-    errorMessage: errorMessage,
-    errorState: errState,
   };
   /* this hook returns an object with two properties 'info' which stores an array of data 
   and 'loadingStatus' which stores the loading status */
